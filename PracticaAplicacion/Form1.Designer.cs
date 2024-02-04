71bc05c57cdfb9d4bf345ec8a56af3afcb99c94e
Karol Estanislao Romero Planissig <karolplanissigwsp123@gmail.com>
2024-02-04 15:53:03 -0300

    primer subida

---
+++ PracticaAplicacion/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace PracticaAplicacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApe = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lblDire = new System.Windows.Forms.Label();
+            this.lblResul = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnAcep = new System.Windows.Forms.Button();
+            this.btnCance = new System.Windows.Forms.Button();
+            this.txtMostrar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblApe
+            // 
+            this.lblApe.AutoSize = true;
+            this.lblApe.Location = new System.Drawing.Point(168, 46);
+            this.lblApe.Name = "lblApe";
+            this.lblApe.Size = new System.Drawing.Size(60, 16);
+            this.lblApe.TabIndex = 0;
+            this.lblApe.Text = "Apellido:";
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(169, 94);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(59, 16);
+            this.lblNom.TabIndex = 1;
+            this.lblNom.Text = "Nombre:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(169, 135);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(43, 16);
+            this.lblEdad.TabIndex = 2;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // lblDire
+            // 
+            this.lblDire.AutoSize = true;
+            this.lblDire.Location = new System.Drawing.Point(168, 174);
+            this.lblDire.Name = "lblDire";
+            this.lblDire.Size = new System.Drawing.Size(67, 16);
+            this.lblDire.TabIndex = 3;
+            this.lblDire.Text = "Dirección:";
+            // 
+            // lblResul
+            // 
+            this.lblResul.AutoSize = true;
+            this.lblResul.Location = new System.Drawing.Point(169, 213);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(75, 16);
+            this.lblResul.TabIndex = 4;
+            this.lblResul.Text = "Resultado: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Location = new System.Drawing.Point(313, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox2.Location = new System.Drawing.Point(313, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox3.Location = new System.Drawing.Point(313, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox4.Location = new System.Drawing.Point(313, 174);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // btnAcep
+            // 
+            this.btnAcep.Location = new System.Drawing.Point(214, 368);
+            this.btnAcep.Name = "btnAcep";
+            this.btnAcep.Size = new System.Drawing.Size(75, 23);
+            this.btnAcep.TabIndex = 9;
+            this.btnAcep.Text = "Aceptar";
+            this.btnAcep.UseVisualStyleBackColor = true;
+            this.btnAcep.Click += new System.EventHandler(this.btnAcep_Click);
+            // 
+            // btnCance
+            // 
+            this.btnCance.Location = new System.Drawing.Point(407, 368);
+            this.btnCance.Name = "btnCance";
+            this.btnCance.Size = new System.Drawing.Size(75, 23);
+            this.btnCance.TabIndex = 10;
+            this.btnCance.Text = "Cancelar";
+            this.btnCance.UseVisualStyleBackColor = true;
+            this.btnCance.Click += new System.EventHandler(this.btnCance_Click);
+            // 
+            // txtMostrar
+            // 
+            this.txtMostrar.Location = new System.Drawing.Point(313, 213);
+            this.txtMostrar.Multiline = true;
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(295, 128);
+            this.txtMostrar.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMostrar);
+            this.Controls.Add(this.btnCance);
+            this.Controls.Add(this.btnAcep);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.lblDire);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.lblApe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos personales";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApe;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.Label lblDire;
+        private System.Windows.Forms.Label lblResul;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnAcep;
+        private System.Windows.Forms.Button btnCance;
+        private System.Windows.Forms.TextBox txtMostrar;
+    }
+}
+
---
+++ PracticaAplicacion/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaAplicacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcep_Click(object sender, EventArgs e)
+        {
+            string Apellido = textBox1.Text;
+            string Nombre = textBox2.Text;
+            string Edad = textBox3.Text;
+            string Direccion = textBox4.Text; 
+            if (textBox1.Text == "")
+            {
+                textBox1.BackColor = Color.Red; 
+
+            }
+            else
+            {
+                textBox1.BackColor = System.Drawing.SystemColors.Control; 
+            }
+            if (textBox2.Text == "")
+            {
+                textBox2.BackColor = Color.Red;
+            }
+            else
+            {
+                textBox2.BackColor = System.Drawing.SystemColors.Control; 
+            }
+            if(textBox3.Text == "")
+            {
+                textBox3.BackColor = Color.Red;
+            }
+            else
+            {
+                textBox3.BackColor = System.Drawing.SystemColors.Control; 
+            }
+            if (textBox4.Text == "")
+            {
+                textBox4.BackColor = Color.Red; 
+            }
+            else
+            {
+                textBox4.BackColor = System.Drawing.SystemColors.Control; 
+            }
+            txtMostrar.Text = "Apellido y nombre" + Apellido + " " + Nombre +
+                                "Edad: " + Edad + " Dirección: " + Direccion; 
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCance_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           
+        }
+    }
+}
